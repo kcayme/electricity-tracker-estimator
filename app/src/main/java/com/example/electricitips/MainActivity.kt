7e46cc193071319c74aa5486e2b397806642b85a
Karl <kcayme@gmail.com>
2022-07-22 15:44:06 +0800

    >home and dashboard class edited to be similar with tips and links >upated with raiza's layout

--- app/src/main/java/com/example/electricitips/MainActivity.kt
+++ app/src/main/java/com/example/electricitips/MainActivity.kt
@@ -2,39 +2,46 @@ package com.example.electricitips
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.inputmethod.InputBinding
 import android.widget.ArrayAdapter
-import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
-import androidx.fragment.app.Fragment
 import androidx.navigation.NavController
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupWithNavController
 import com.example.electricitips.databinding.ActivityMainBinding
-import com.example.electricitips.fragments.*
 import com.example.electricitips.databinding.FragmentInputFormBinding
-import com.example.electricitips.fragments.Articles
-import com.example.electricitips.fragments.Dashboard
-import com.example.electricitips.fragments.Home
-import com.example.electricitips.fragments.Others
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        // setup navigation bar
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         //navController controls the navigation between fragments
         navController = navHostFragment.findNavController()
+        appBarConfiguration = AppBarConfiguration(
+            setOf(R.id.home,R.id.dashboard,R.id.links,R.id.tips)
+        )
         binding.bottomNavView.setupWithNavController(navController)
+
+        // listener still needed to ensure correct navigation
+        binding.bottomNavView.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.home -> navController.navigate(R.id.home)
+                R.id.dashboard -> navController.navigate(R.id.dashboard)
+                R.id.links -> navController.navigate(R.id.links)
+                else -> navController.navigate(R.id.tips)
+            }
+            true
+        }
 
         binding.floating.setOnClickListener {
             navController.navigate(R.id.dashboard)
--- app/src/main/java/com/example/electricitips/fragments/Links.kt
+++ app/src/main/java/com/example/electricitips/fragments/Links.kt
@@ -6,7 +6,6 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.electricitips.R
-import com.example.electricitips.databinding.FragmentLinksBinding
 
 class Links :  Fragment(){
     override fun onCreateView(
