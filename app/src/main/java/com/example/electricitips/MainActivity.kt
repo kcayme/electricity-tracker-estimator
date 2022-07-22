53f86b5a5c3410e9bdf96f171d4f57689df8bc5e
Karl <kcayme@gmail.com>
2022-07-22 15:08:52 +0800

    utilized navigation controller, floating button added onclickListener

--- app/src/main/java/com/example/electricitips/MainActivity.kt
+++ app/src/main/java/com/example/electricitips/MainActivity.kt
@@ -2,43 +2,55 @@ package com.example.electricitips
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.inputmethod.InputBinding
+import android.widget.ArrayAdapter
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
 import com.example.electricitips.databinding.ActivityMainBinding
 import com.example.electricitips.fragments.*
+import com.example.electricitips.databinding.FragmentInputFormBinding
+import com.example.electricitips.fragments.Articles
+import com.example.electricitips.fragments.Dashboard
+import com.example.electricitips.fragments.Home
+import com.example.electricitips.fragments.Others
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    // create instances of the fragment objects
-    private val homeFragment = Home()
-    private val dashboardFragment = Dashboard()
-    private val linksFragment = Links()
-    private val tipsFragment = Tips()
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        replaceFragment(homeFragment)
 
-        // listener on the navigation bar
-        binding.bottomNavView.setOnItemSelectedListener {
-            // switch-case the id of the item selected in the navigation bar
-            when(it.itemId){
-                R.id.home -> replaceFragment(homeFragment)
-                R.id.dashboard -> replaceFragment(dashboardFragment)
-                R.id.links -> replaceFragment(linksFragment)
-                R.id.tips -> replaceFragment(tipsFragment)
-            }
-            true
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        //navController controls the navigation between fragments
+        navController = navHostFragment.findNavController()
+        binding.bottomNavView.setupWithNavController(navController)
+
+        binding.floating.setOnClickListener {
+            navController.navigate(R.id.dashboard)
+
+            val inputBind = FragmentInputFormBinding.inflate(layoutInflater)
+            val items: Array<String> = resources.getStringArray(R.array.appliance_types)
+            val typesAdapter = ArrayAdapter(this, R.layout.dropdown_appliance_types, items)
+            inputBind.autoCompleteTypes.setAdapter(typesAdapter)
+
+            val mBuilder = AlertDialog.Builder(this)
+                .setView(inputBind.root)
+                .setTitle("Login Form")
+            val mAlertDialog = mBuilder.show()
         }
+
+
     }
-    // function for replacing the view when navigation button is selected
-    private fun replaceFragment(fragment: Fragment){
-        if(fragment != null){
-            val transaction = supportFragmentManager.beginTransaction()
-            transaction.replace(R.id.fragmentContainer, fragment)
-            transaction.commit()
-        }
-    }
+
 }
